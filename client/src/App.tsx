6b45ab6e654c8d39e470c92bb4086dfda0d03bd6
Frostyy890 <abdurahmon2004nuriy@gmail.com>
2024-06-12 01:55:40 +0500

    initialized client side with react-vite

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return <Button className="bg-blue-500 hover:bg-blue-400">Hello world!</Button>;
+}
+
+export default App;
---
+++ client/src/pages/auth/components/AuthForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthForm = () => {
+  return <form></form>;
+};
+
+export default AuthForm;
