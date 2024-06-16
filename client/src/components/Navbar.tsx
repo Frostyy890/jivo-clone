ab8b3ad949422356b55596d21bd5460ba5ffd973
Frostyy890 <abdurahmon2004nuriy@gmail.com>
2024-06-16 18:00:32 +0500

    feat: made minor adjustments at navabar and sign-up form

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="w-full fixed inset-x-0 top-0 z-50 bg-white border shadow-sm">
+    <nav className="w-full fixed inset-x-0 top-0 z-50 bg-white border shadow-sm rounded-b-sm">
       <div className="flex items-center justify-between h-14 py-2 px-8">
         {/* Logo */}
         <a href="/" className="text-2xl font-semibold">
@@ -67,12 +67,7 @@ function NavLink({ path, name }: { path: string; name: string }) {
   const resolvedPath = useResolvedPath(path);
   const isOnPage = useMatch({ path: resolvedPath.pathname, end: true });
   return (
-    <a
-      href={path}
-      className={`hover:underline underline-offset-2 ${
-        isOnPage ? "underline" : ""
-      }`}
-    >
+    <a href={path} className={`hover:underline underline-offset-2 ${isOnPage ? "underline" : ""}`}>
       {name}
     </a>
   );
--- client/src/pages/auth/components/SignUpForm.tsx
+++ client/src/pages/auth/components/SignUpForm.tsx
@@ -1,24 +1,14 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const signUpSchema = z.object({
-  username: z
-    .string()
-    .min(6, { message: "Username must be at least 6 characters long" }),
+  username: z.string().min(6, { message: "Username must be at least 6 characters long" }),
   email: z.string().email({ message: "Email must be valid" }),
-  password: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters long" }),
+  password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
 });
 
 type SignUpFormData = z.infer<typeof signUpSchema>;
@@ -61,12 +51,8 @@ const SignUpForm = () => {
             )}
           ></FormField>
         ))}
-        <Button
-          className="w-full mt-1 font-semibold"
-          type="submit"
-          disabled={!isDirty || !isValid}
-        >
-          Create Account
+        <Button className="w-full mt-1 font-semibold" type="submit" disabled={!isDirty || !isValid}>
+          Sign up with Email
         </Button>
       </form>
     </Form>
